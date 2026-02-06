0c74d341c808090d8985051c0fbe7df9ac389b13
Pratik Vilas Gadhe <pratikgadhe366@gmail.com>
2026-02-06 12:23:54 +0530

    Day 1 of Python

--- python-for-ai/sales-analysis/analyzer.py
+++ python-for-ai/sales-analysis/analyzer.py
@@ -1,4 +1,4 @@
-# analyzer.py
+
 import pandas as pd
 from helpers import calculate_total, format_currency
 
@@ -24,3 +24,4 @@ for index, row in df.iterrows():
 grand_total = df['total'].sum()
 formatted_grand_total = format_currency(grand_total)
 print(f"\nGrand Total: {formatted_grand_total}")
+print(grand_total)
